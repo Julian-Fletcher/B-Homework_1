b2f03cfe1f587c2216af63fa951350b97c19d86d
Ahmad Ennin <121472038+ARockEnnin@users.noreply.github.com>
2024-01-31 14:29:32 -0600

    Homework_1.java

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -5,7 +5,7 @@ public class Homework_1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Name: Julian ID: 37055413"); //Shouldn't this be our Git User ID??
-		System.out.println("Name: Ahmad Ennin. GitHubID: ARockEnnin");
+		System.out.println("Name: Ahmad Ennin. GitHubID: 121472038");
 	}
 
 }
