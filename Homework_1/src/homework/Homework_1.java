fa6c29eed2bacc540142b9b45a84f18cfbe263da
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-01-31 15:09:09 -0600

    Update Homework_1.java

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -6,6 +6,7 @@ public class Homework_1 {
 	{
 		System.out.println("Name: Julian ID: 37055413"); //Shouldn't this be our Git User ID??
 		System.out.println("Name: Ahmad Ennin. GitHubID: ARockEnnin");
+		System.out.println("Name: Braeden Songer, ID: 149883938");
 	}
 
 }
