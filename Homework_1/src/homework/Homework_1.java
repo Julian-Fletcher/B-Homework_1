3d918190d3bc02ac09625610f27887d57a6379c2
Ahmad Ennin <121472038+ARockEnnin@users.noreply.github.com>
2024-01-31 14:18:16 -0600

    Update Homework_1.java

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -3,7 +3,7 @@ package homework;
 public class Homework_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Name: Julian ID: 37055413");
+		System.out.println("Name: Julian ID: 37055413"); //Shouldn't this be our Git User ID??
 
 	}
 
