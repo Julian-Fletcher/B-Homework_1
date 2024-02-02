bd1088701c73a406fc23bdda17ed584d01c7b550
Julian Fletcher <37055413+Julian-Fletcher@users.noreply.github.com>
2024-02-02 14:46:35 -0600

    Julian added his last name
    he forgot his last name

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -4,7 +4,7 @@ public class Homework_1 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Name: Julian ID: 37055413"); //Shouldn't this be our Git User ID??
+		System.out.println("Name: Julian Fletcher: ID: 37055413"); 
 		System.out.println("Name: Ahmad Ennin. GitHubID: ARockEnnin");
 		System.out.println("Name: Braeden Songer, ID: 149883938");
 	}
