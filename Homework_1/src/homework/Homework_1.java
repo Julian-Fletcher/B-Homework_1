59af5751f5f7f51f2c195ea50deee4098016aaa5
TosinO25 <113729630+TosinO25@users.noreply.github.com>
2024-02-02 20:06:20 -0600

    Update Homework_1.java

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -7,7 +7,7 @@ public class Homework_1 {
 		System.out.println("Name: Julian Fletcher: ID: 37055413"); 
 		System.out.println("Name: Ahmad Ennin. GitHubID: ARockEnnin");
 		System.out.println("Name: Braeden Songer, ID: 149883938");
-		System.out.println("Name: Tosin Ogungbade, GitHubID: TosinO25);
+		System.out.println("Name: Tosin Ogungbade, GitHubID: TosinO25");
 
 	}
 
