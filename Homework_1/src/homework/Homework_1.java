9cb38680ff8cc6944d79887b8910833dc11e573a
ahmad <ahmad@10.7.24.207>
2024-02-05 14:00:27 -0600

    ID instead of username

--- Homework_1/src/homework/Homework_1.java
+++ Homework_1/src/homework/Homework_1.java
@@ -5,7 +5,7 @@ public class Homework_1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Name: Julian Fletcher: ID: 37055413"); 
-		System.out.println("Name: Ahmad Ennin. GitHubID: ARockEnnin");
+		System.out.println("Name: Ahmad Ennin. GitHubID: 121472038");
 		System.out.println("Name: Braeden Songer, ID: 149883938");
 		System.out.println("Name: Tosin Ogungbade, GitHubID: TosinO25");
 
